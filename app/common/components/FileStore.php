03ce7874f267bf1248069a3530a0366573599769
Kefah Issa <kefah.issa@gmail.com>
2015-07-19 16:10:41 +0100

    Improved FileStore

--- app/common/components/FileStore.php
+++ app/common/components/FileStore.php
@@ -8,23 +8,25 @@ use \yii\base\InvalidConfigException;
 class FileStore extends Component {
 	public $path;
   public $ignore = ['.', '..', 'index.sqlite3'];
+  public $index_path;
   private $db;
   private $command;
   private $finfo;
-  private $stats = ['total'=>0, 'folders'=>0, 'files'=>0, 'other'=>0];
 
   public function init() {
-    $this->db = new \yii\db\Connection (['dsn' => "sqlite:{$this->path}/index.sqlite3"]);
+// Make sure you have read access to all files chmod -R . g+r , and read/execute access to all folders: find ./ -type d -exec chmod g+x {} \;
+		echo "Initializing Filestore, {$this->index_path}\n";
+    $this->db = new \yii\db\Connection (['dsn' => "sqlite:{$this->index_path}/index.sqlite3"]);
     $this->db->open();
 		$ddl_sql = <<<EOL
 			DROP TABLE IF EXISTS Entry;
 			CREATE TABLE IF NOT EXISTS Entry (
 				id INTEGER PRIMARY KEY,
-				embdedded BLOB NULL,
+				embdedded BLOB NULL, -- if this is null then its an external file
 				path TEXT NULL,
         name TEXT NULL,
-				checksum TEXT NOT NULL,
-				'type' TEXT NOT NULL,
+				checksum TEXT NULL, -- make it not null later
+				-- 'type' TEXT NOT NULL,
 				byte_size INTEGER NOT NULL,
 				format TEXT NULL, -- Mime
 				device INTEGER NULL, -- Device id. Null in non-linux
@@ -37,7 +39,7 @@ class FileStore extends Component {
 				modify_time INTEGER,
 				block_size INTEGER, -- Could be null on non-linux
 				blocks INTEGER, -- Could be null on non-linux
-				UNIQUE (path, name)
+				UNIQUE (path,name)
 			);
 		CREATE INDEX entry_checksum on Entry (checksum);
 EOL;
@@ -54,16 +56,112 @@ EOL;
     if(isset($this->finfo)) finfo_close($this->finfo);
 	}
 
-	public function index($path = '.') { // Indexes all files/folders under $this->path 
+  /*private $stats = ['total'=>0, 'folders'=>0, 'files'=>0, 'skips'=>0];
+	private function stats ($type) {
+			switch($type) {
+				case 'directory':
+					$this->stats['folders'] +=1;
+					break;
+				case 'regular':
+					$this->stats['files'] +=1;
+					break;
+				default:
+					$this->stats['skips'] +=1;
+					break;
+			}
+			$this->stats['total']+=1;
+	}*/
+
+  public function index() {
+		$cmd = 'find ./ -type f ' ;
+		$last = $this->index_path . 'index.last';
+		if(file_exists($last)) 
+			$cmd .= " -newer '$last'";
+
+		$descs = array(0=>array('pipe','r'),1=>array('pipe','w'),2=>array('pipe','w'));
+    echo "cmd $cmd ; path {$this->path}\n";
+		$p = proc_open($cmd, $descs, $pipes, $this->path, array());
+		if (is_resource($p)) {
+			$count = 0;
+			$transaction = $this->db->beginTransaction();
+			$start = time();
+			try {
+				while ($one = fgets($pipes[1])) {
+					$one = substr($one,0,-1); // Remove the trailing newline
+					$one_pathname= $this->path . $one;
+					if(++$count % 1000 == 0) {
+						$elapsed = time() - $start;
+						echo "\rCount $count/$elapsed";
+					}
+					if(!is_file($one_pathname)){ 
+						echo "\nSkipping file $one_pathname not a file\n";
+						continue;
+					}
+
+					$previous = error_reporting(E_ERROR); // Suppress E_WARNING caused by lstat
+					$stat = lstat($one_pathname);
+					error_reporting($previous);
+					if(empty($stat) ) {
+						echo "\nSkipping file $one_pathname lstat failed\n";
+						continue;
+					}
+					$info = pathinfo($one);
+					$data = array();
+					$data['path'] = $info['dirname'];
+					$data['name']= $info['basename'];
+					$data['byte_size'] = $stat['size'];
+					$data['device'] = $stat['dev'];
+					$data['inode'] = $stat['ino'];
+					$data['nlink'] = $stat['nlink'];
+					$data['uid'] = $stat['uid'];
+					$data['gid'] = $stat['gid'];
+					$data['access_time'] = $stat['atime'];
+					$data['modify_time'] = $stat['mtime'];
+					$data['create_time'] = $stat['ctime'];
+					$data['block_size'] = $stat['blksize'];
+					$data['blocks'] = $stat['blocks'];
+					// $data['checksum'] = hash_file('sha256', $one_pathname);
+					/*$mime = finfo_file($this->finfo, $one);
+					echo "Entry: $item type: $mime \n";
+					$data['format'] = $mime;*/
+
+					$this->command->insert('Entry', $data)->execute();
+				}
+				echo "\nCommitting transaction\n";
+				$transaction->commit();
+			} catch (Exception $e) {
+				$transaction->rollback();
+				throw $e;
+			}
+			echo "\nCompleted processing $count file\n";
+			$count = 0;
+			while ($one = fgets($pipes[2])) {
+				$count++;
+			}
+			if($count >0)
+				echo "Failed to process $count files\n";
+			fclose($pipes[0]);
+			fclose($pipes[1]);
+			fclose($pipes[2]);
+			$return = proc_close($p);
+			echo "Command returnd $return\n";
+		}
+		if(!touch($last)) 
+			echo "Touching file failed $last\n";
+	}
+
+	public function index_old($path = '.') { // Indexes all files/folders under $this->path 
 		$list = scandir("{$this->path}/$path" );
     echo "processing path $path\n";
     foreach($list as $item) {
 			if(in_array($item, $this->ignore))
 				continue;
 			$one = "{$this->path}/$path/$item";
-      $stat = stat($one);
-      $mime = finfo_file($this->finfo, $one);
-      echo "Entry: $item type: $mime \n";
+      $stat = lstat($one);
+			$mode = file_type($stat['mode']);
+      //$this->stats($mode);
+      if($mode != 'directory' && $mode != 'regular')
+				continue; // Skip non-file/folder
 			
 			/*print_r($stat);
 			$attrs = xattr_list($one);
@@ -74,7 +172,6 @@ EOL;
 			$data['path'] = $path;
       $data['name']= $item;
 			$data['byte_size'] = $stat['size'];
-			$data['format'] = $mime;
 			$data['device'] = $stat['dev'];
 			$data['inode'] = $stat['ino'];
 			$data['nlink'] = $stat['nlink'];
@@ -86,22 +183,12 @@ EOL;
 			$data['block_size'] = $stat['blksize'];
 			$data['blocks'] = $stat['blocks'];
       $data['checksum'] = hash_file('sha256', $one);
- 			$data['type'] = file_type($stat['mode']);
+ 			$data['type'] = $mode;
+      $mime = finfo_file($this->finfo, $one);
+      echo "Entry: $item type: $mime \n";
+			$data['format'] = $mime;
 
 			$this->command->insert('Entry', $data)->execute();
-			//print_r($data);
-			switch($data['type']) {
-				case 'directory':
-					$this->stats['folders'] +=1;
-					break;
-				case 'regular':
-					$this->stats['files'] +=1;
-					break;
-				default:
-					$this->stats['other'] +=1;
-					break;
-			}
-			$this->stats['total']+=1;
       echo "Processed {$this->stats['files']} files,  {$this->stats['folders']} folders out of  {$this->stats['total']}\n";
 			if('directory' == $data['type'])
 				$this->index("{$path}/{$item}");
